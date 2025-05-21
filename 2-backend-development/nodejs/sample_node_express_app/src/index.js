89a0eed2edcc9acccbe1609fb5c251190c0b1bde
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-05-21 13:10:57 +1000

    Add basic node/express app code

---
+++ 2-backend-development/nodejs/sample_node_express_app/src/index.js
@@ -0,0 +1,22 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json()); // Parses application/json
+app.use(urlencoded({ extended: true })); // Parses application/x-www-form-urlencoded
+app.use(cors({ origin: process.env.CORS_URL }));
+
+app.use((err, req, res, next) => {
+  res
+    .status(500)
+    .json({ message: "Something Went Wrong !", error: err.message });
+});
+
+app.listen(port, () => {
+  console.log("Messaging App is running on http://localhost:", port);
+});
